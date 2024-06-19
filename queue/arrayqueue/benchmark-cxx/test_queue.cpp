6c2d70426e88a0d514477ec16cda0ed7c44904b8
hexu <hexu_1985@sina.com>
2024-06-19 18:51:05 +0800

    add queue/arrayqueue/benchmark-cxx

---
+++ queue/arrayqueue/benchmark-cxx/test_queue.cpp
@@ -0,0 +1,21 @@
+#include <queue>
+#include <iostream>
+
+void test_queue1() {
+    std::queue<int> myqueue;
+    int sum (0);
+
+    for (int i=1;i<=10;i++) myqueue.push(i);
+
+    while (!myqueue.empty())
+    {
+        sum += myqueue.front();
+        myqueue.pop();
+    }
+
+    std::cout << "total: " << sum << '\n';
+}
+
+int main() {
+    test_queue1();
+}
