f9f2dbb143a76804f01b12a4aad15d8cb8b85846
hexu <hexu_1985@sina.com>
2024-06-20 10:51:19 +0800

    update queue/arrayqueue

--- queue/arrayqueue/benchmark-cxx/test_queue.cpp
+++ queue/arrayqueue/benchmark-cxx/test_queue.cpp
@@ -16,6 +16,17 @@ void test_queue1() {
     std::cout << "total: " << sum << '\n';
 }
 
+void test_queue2() {
+    std::queue<int> myints;
+    std::cout << "0. size: " << myints.size() << '\n';
+
+    for (int i=0; i<5; i++) myints.push(i);
+    std::cout << "1. size: " << myints.size() << '\n';
+
+    myints.pop();
+    std::cout << "2. size: " << myints.size() << '\n';
+}
+
 int main() {
     test_queue1();
 }
--- queue/arrayqueue/python/test_queue.py
+++ queue/arrayqueue/python/test_queue.py
@@ -2,7 +2,7 @@
 
 from arrayqueue import ArrayQueue as Queue
 
-def test_stack1():
+def test_queue1():
     myqueue = Queue()
     sum = 0
     for i in range(1, 11):
@@ -13,5 +13,17 @@ def test_stack1():
 
     print("total:", sum)
 
+def test_queue2():
+    myints = Queue()
+    print("0. size:", len(myints))
+
+    for i in range(5):
+        myints.add(i)
+    print("1. size:", len(myints))
+
+    myints.pop()
+    print("2. size:", len(myints))
+
 if __name__ == "__main__":
-    test_stack1()
+    test_queue1()
+    test_queue2()
