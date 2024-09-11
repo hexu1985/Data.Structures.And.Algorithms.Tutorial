2cbf54c61132b276d32d8e603e49109368a45baf
hexu <hexu_1985@sina.com>
2024-09-11 17:53:49 +0800

    update selection_tree/benchmark

--- selection_tree/benchmark/n_way_merge.cpp
+++ selection_tree/benchmark/n_way_merge.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include "random.hpp"
+
+Random my_random{0};
+bool dump = true;
+
+std::vector<int> generate_vector(int size) {
+    int min = 1;
+    int max = 1000;
+    std::vector<int> vec;
+    for (int i = 0; i < size; i++) {
+        vec.push_back(my_random.randint(min, max));
+    }
+    return vec;
+}
+
+void print_vector(std::vector<int>& vec) {
+    for (auto& i : vec) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+}
+
+void n_way_merge(std::vector<int>& out, const std::vector<std::vector<int>>& queues) {
+    out.clear();
+    for (auto& q : queues) {
+        for (auto& v : q) {
+            out.push_back(v);
+        }
+    }
+    std::sort(out.begin(), out.end());
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -23,5 +55,29 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    std::vector<std::vector<int>> queues;
+    for (int i = 0; i < number_of_queue; i++) {
+        std::vector<int> vec = generate_vector(queue_size);
+        std::sort(vec.begin(), vec.end());
+        if (dump) {
+            std::cout << "input[" << i+1 << "]: ";
+            print_vector(vec);
+        }
+        queues.push_back(vec);
+    }
 
+    std::vector<int> result;
+    n_way_merge(result, queues);
+    if (dump) {
+        std::cout << "result: ";
+        print_vector(result);
+    }
+
+    if (std::is_sorted(result.begin(), result.end())) {
+        std::cout << "merge success!" << std::endl;
+    } else {
+        std::cout << "merge failed!" << std::endl;
+    }
+
+    return 0;
 }
