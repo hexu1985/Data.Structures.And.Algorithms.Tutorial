ad8e9edba2365efd93f12e0f29be4ea4133f8128
hexu <hexu_1985@sina.com>
2024-09-27 10:39:44 +0800

    add linked_directed_graph/recipe-03

---
+++ linked_directed_graph/recipe-03/python/algorithms.py
@@ -0,0 +1,33 @@
+"""
+File: algorithms.py
+
+Graph processing algorithms
+"""
+
+import collections
+
+def topoSort(g, startLabel = None):  
+    lyst = collections.deque()
+    g.clearVertexMarks()
+    for v in g.vertices():
+        if not v.isMarked():
+            _topoDfs(g, v, lyst)
+
+    return lyst
+
+def _topoDfs(g, v, lyst):
+    v.setMark()
+    for w in g.neighboringVertices(v.getLabel()):
+        if not w.isMarked():
+            _topoDfs(g, w, lyst)
+    lyst.appendleft(v)
+
+
+def shortestPaths(g, startLabel):
+    # Exercise
+    return ["Under development"]
+
+
+def hasCycles(g):
+    pass
+
